ff00c66c438feec7587f6f4103bbe4619911fdad
GrigoriyLyullin <grisha.lyullin@gmail.com>
2014-11-02 16:47:45 +0400

    Нчало работы на лабораторной работой номер 3 на основе работы номер 2.

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,6 @@ public class Main {
     public static void main(String[] args) {
         DrawPanel drawPanel = new DrawPanel(WIDTH, HEIGHT);
         ControlPanel controlPanel = new ControlPanel(drawPanel, POINT_COUNT);
-        new MainWindow("LabWork2", WIDTH, HEIGHT, drawPanel, controlPanel);
+        new MainWindow("LabWork3", WIDTH, HEIGHT, drawPanel, controlPanel);
     }
 }
